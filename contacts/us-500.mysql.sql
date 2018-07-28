be2ef04228ed9d485e49f5aafe800d4dd02abf75
steve.tarver <steve.tarver@gmail.com>
2018-07-28 00:29:41 -0600

    Add mariadb
    Also:
    - add 'test' account creation to contacts/us-500.mysql.sql
    - update percona to use new mysql db init

--- contacts/us-500.mysql.sql
+++ contacts/us-500.mysql.sql
@@ -1,3 +1,13 @@
+-- Create the 'test' schema and a dedicated user
+-- NOTE: using this mechanism because MYSQL_DATABASE/USER/PASSWORD does not appear to work as described in
+--       https://hub.docker.com/_/percona/
+
+CREATE USER 'test'@'%' IDENTIFIED BY 'test';
+CREATE SCHEMA IF NOT EXISTS test;
+GRANT ALL ON test.* TO 'test'@'%';
+
+USE test;
+
 -- MySQL dump 10.13  Distrib 5.6.19, for osx10.7 (i386)
 --
 -- Host: 127.0.0.1    Database: test
